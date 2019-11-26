b494a38470bf5fc0cf5b81db11e1b76374822e79
NKragh <39466838+NKragh@users.noreply.github.com>
2019-11-26 13:36:28 +0100

    Humidity regulator er nu også implementeret
    Lavet som en del af temp regulator. Kan deles op hvis det ønskes.

--- GreenPiHouseSenseHat/SenseHatRegulator.py
+++ GreenPiHouseSenseHat/SenseHatRegulator.py
@@ -5,6 +5,7 @@ s = SenseHat()
 
 O = (0,0,0)
 X = (200,200,200)
+R = (255,0,0)
 
 logo_up = [
     O, O, O, X, X, O, O, O,
@@ -28,19 +29,28 @@ logo_down = [
     O, O, O, X, X, O, O, O,
 ]
 
+print("Indtast den optimale temperatur: ")
 optimal_temp = int(input())
+print("Indtast den optimale luftfugtighed: ")
+optimal_hum = int(input())
 
 while True: 
     temp = int(s.get_temperature())
+    hum = int(s.get_humidity())
     
-    if temp == optimal_temp:
+    if temp == optimal_temp and hum == optimal_hum:
       print(" ")
       s.clear()
-    elif temp > optimal_temp:
+    elif temp > optimal_temp or hum > optimal_hum:
       print("Over")
       s.set_pixels(logo_up)
-    else:
+
+    elif temp < optimal_temp or hum < optimal_hum:
       print("Under")
       s.set_pixels(logo_down)
+
+    else:
+        print("Fejl")
+        s.clear(R)
 
     time.sleep(.75)
