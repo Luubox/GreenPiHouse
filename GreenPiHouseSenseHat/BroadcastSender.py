5ae687bd27e8d931b9d24c10a5e4677b55f0fcb7
jonasras1994 <jona72c5@edu.easj.dk>
2019-11-27 09:29:51 +0100

    Rettet temperatur og humidity, samt ændret port#

--- GreenPiHouseSenseHat/BroadcastSender.py
+++ GreenPiHouseSenseHat/BroadcastSender.py
@@ -1,22 +1,32 @@
-BROADCAST_TO_PORT = 7000
+BROADCAST_TO_PORT = 10100
 from sense_hat import SenseHat
 import time
 from socket import *
 from datetime import datetime
 
 s = SenseHat()
-temp = s.get_temperature()
-print("Temperature: %.0f C" % temp)
 
-humidity = s.get_humidity()
-print("Humidity: %.0f %" % humidity)
+while True:
+
+  temp = s.get_temperature()
+  humi = s.get_humidity()
+  
+  temp = round(temp, 1)
+  humi = round(humi, 1)
+  
+  print("Temperature: " + str(temp) + " C")
+  print("Humidity: " + str(humi) + " %")
+  
+  time.sleep(60)
 
 s = socket(AF_INET, SOCK_DGRAM)
 #s.bind(('', 14593))     # (ip, port)
 # no explicit bind: will bind to default IP + random port
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
 while True:
-	data = "Current time " + str(datetime.now())
+	data = "Current temperature " + str(temp())
+	data = "Current humidity " + str(humi))
 	s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
 	print(data)
 	time.sleep(1)
