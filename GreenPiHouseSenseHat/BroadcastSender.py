77057376fbfa800f75ebb86d47e182398cf0cb20
NKragh <39466838+NKragh@users.noreply.github.com>
2019-11-27 14:49:33 +0100

    Mulig løsning,
    der nok ikke virker. UDP-mæssigt.

--- GreenPiHouseSenseHat/BroadcastSender.py
+++ GreenPiHouseSenseHat/BroadcastSender.py
@@ -5,7 +5,7 @@ from socket import *
 from datetime import datetime
 
 s = SenseHat()
-'''
+
 while True:
 
   temp = s.get_temperature()
@@ -18,7 +18,6 @@ while True:
   print("Humidity: " + str(humi) + " %")
   
   time.sleep(3600) 
-  '''
 
 s = socket(AF_INET, SOCK_DGRAM)
 #s.bind(('', 14593))     # (ip, port)
@@ -30,3 +29,45 @@ while True:
 	s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
 	print(data)
 	time.sleep(1)
+
+
+
+#Mulig løsning til hele koden på Raspberry Pi
+
+def lyt():
+	UDP_IP = "127.0.0.1"
+	UDP_PORT = 5005
+
+	sock = socket.socket(socket.AF_INET, # Internet
+						socket.SOCK_DGRAM) # UDP
+	sock.bind((UDP_IP, UDP_PORT))
+
+	while True:
+		data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+		print "received message:", data
+
+def broadcast():
+
+
+i = 0
+while True:
+	lyt()
+	if i == 3600:
+		broadcast()
+		i = 0
+	
+	if temp == optimal_temp and hum == optimal_hum:
+      print(" ")
+      s.clear()
+    elif temp > optimal_temp or hum > optimal_hum:
+      print("Over")
+      s.set_pixels(logo_up)
+    elif temp < optimal_temp or hum < optimal_hum:
+      print("Under")
+      s.set_pixels(logo_down)
+    else:
+        print("Fejl")
+        s.clear(R)
+	sleep(1)
+	i += 1
+
