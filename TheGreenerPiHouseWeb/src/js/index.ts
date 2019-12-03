18985475064d3c75a2824e4bfe1e9538e29f731c
therightpathy <p_razmussen@hotmail.com>
2019-12-03 13:01:29 +0100

    Enige

--- TheGreenerPiHouseWeb/src/js/index.ts
+++ TheGreenerPiHouseWeb/src/js/index.ts
@@ -1,12 +1,31 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index";
+interface Idata {
+    sensorName: string, 
+    temperature: number,
+    co2: number
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
+let elementTemp: HTMLDivElement = <HTMLDivElement> (document.getElementById("temperature"))
+let elementHumi: HTMLDivElement = <HTMLDivElement> (document.getElementById("humidity"))
+let elementButton: HTMLButtonElement = <HTMLButtonElement> (document.getElementById("startbutton"))
+elementButton.addEventListener("click", start)
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+function start (){
+    console.log("Hej")
+    axios.get <Idata []> ("https://thegreenerpihouse.azurewebsites.net/api/data")
+    .then(function (response: AxiosResponse < Idata[] > ): void {
+        let result: string = "<ul>";
+        console.log(response.data)
+        response.data.forEach((c: Idata) => {
+            result += "<li>" + c.sensorName + " " + c.temperature + " " + c.co2 + "</li>"
+        })
+        result + "</ul>"
+        elementTemp.innerHTML = result
+    })
+    .catch(function (error: AxiosError): void {
+        elementTemp.innerHTML = error.message
+    })
+}
