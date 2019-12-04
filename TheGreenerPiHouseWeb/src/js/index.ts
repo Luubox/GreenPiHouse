0e9b4a925a1ff803636dc00e32625818ec4e41fa
jonasras1994 <jona72c5@edu.easj.dk>
2019-12-04 14:39:09 +0100

    Diverse
    - Korrekt visning af temp og humi fra database (uden knap)
    - Liste af ønskede værdier
    - Korrekte symboler for temp og humi på værdier fra database
    - Start på Grid i CSS

--- TheGreenerPiHouseWeb/src/js/index.ts
+++ TheGreenerPiHouseWeb/src/js/index.ts
@@ -10,26 +10,59 @@ interface Idata {
 
 let elementTemp: HTMLDivElement = <HTMLDivElement> (document.getElementById("temperature"))
 let elementHumi: HTMLDivElement = <HTMLDivElement> (document.getElementById("humidity"))
-let elementButton: HTMLButtonElement = <HTMLButtonElement> (document.getElementById("startbutton"))
-let LatestButton: HTMLButtonElement = <HTMLButtonElement> (document.getElementById("Latestbutton"))
-elementButton.addEventListener("click", GetAll)
-LatestButton.addEventListener("click", GetLatest)
+let elementWindow: HTMLBodyElement = <HTMLBodyElement> (document.getElementById("body"))
+elementWindow.addEventListener("loadend", GetTempterature)
+window.addEventListener("load", (event)=> {
+    GetTempterature()
+    GetHumidity()
+})
 
-function GetAll (){
-    axios.get <Idata []> ("https://thegreenerpihouse.azurewebsites.net/GetAllData")
-    .then(function (response: AxiosResponse < Idata[] > ): void {
-        let result: string = "<ul>";
+//let elementButton: HTMLButtonElement = <HTMLButtonElement> (document.getElementById("startbutton"))
+// let LatestButton: HTMLButtonElement = <HTMLButtonElement> (document.getElementById("Latestbutton"))
+// elementButton.addEventListener("click", GetAll)
+// LatestButton.addEventListener("click", GetLatest)
+
+
+
+function GetTempterature(){
+    axios.get <Idata> ("https://thegreenerpihouse.azurewebsites.net/GetLatestData")
+    .then(function (response: AxiosResponse <Idata>): void {
         console.log(response.data)
-        response.data.forEach((c: Idata) => {
-            result += "<li>Temperature: " + c.temperature + " Humidity: " + c.humidity + "</li>"
-        })
-        result + "</ul>"
+        let result = "Temperature: " + response.data.temperature + "°C"
         elementTemp.innerHTML = result
     })
-    .catch(function (error: AxiosError): void {
+    .catch(function(error: AxiosError): void {
         elementTemp.innerHTML = error.message
     })
 }
+
+function GetHumidity(){
+    axios.get <Idata> ("https://thegreenerpihouse.azurewebsites.net/GetLatestData")
+    .then(function (response: AxiosResponse <Idata>): void{
+        console.log(response.data)
+        let result = "Humidity: " + response.data.humidity + "%"
+        elementHumi.innerHTML = result
+    })
+    .catch(function(error: AxiosError): void {
+        elementHumi.innerHTML  = error.message
+    })
+}
+
+// function GetAll (){
+//     axios.get <Idata []> ("https://thegreenerpihouse.azurewebsites.net/GetAllData")
+//     .then(function (response: AxiosResponse < Idata[] > ): void {
+//         let result: string = "<ul>";
+//         console.log(response.data)
+//         response.data.forEach((c: Idata) => {
+//             result += "<li>Temperature: " + c.temperature + " Humidity: " + c.humidity + "</li>"
+//         })
+//         result + "</ul>"
+//         elementTemp.innerHTML = result
+//     })
+//     .catch(function (error: AxiosError): void {
+//         elementTemp.innerHTML = error.message
+//     })
+// }
 
 function GetLatest() {
     axios.get <Idata> ("https://thegreenerpihouse.azurewebsites.net/GetLatestData")
