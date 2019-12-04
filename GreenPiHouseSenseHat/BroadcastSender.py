16a3a5fc9df47f8d453f855fcea74e7fbc844a76
NKragh <39466838+NKragh@users.noreply.github.com>
2019-12-04 13:59:34 +0100

    Python unittesting

--- GreenPiHouseSenseHat/BroadcastSender.py
+++ GreenPiHouseSenseHat/BroadcastSender.py
@@ -4,63 +4,58 @@ import time
 from socket import *
 from datetime import datetime
 
+optimal_temp = input("Indtast den optimale temperatur: ")
+optimal_hum = input("Indtast den optimale luftfugtighed: ")
+
 s = SenseHat()
 
-while True:
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
 
-  #Måler temperatur
-  temp = s.get_temperature()
-  #Måler luftfugtighed
-  humi = s.get_humidity()
-  
-  temp = round(temp, 1)
-  humi = round(humi, 1)
-  
-  print("Temperature: " + str(temp) + " C")
-  print("Humidity: " + str(humi) + " %")
-  
-  time.sleep(3600) 
+sock = socket(AF_INET, # Internet
+          SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
 
-s = socket(AF_INET, SOCK_DGRAM)
-#s.bind(('', 14593))     # (ip, port)
-# no explicit bind: will bind to default IP + random port
-s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+O = (0,0,0)
+X = (200,200,200)
+R = (255,0,0)
 
-while True:
+logo_up = [
+    O, O, O, X, X, O, O, O,
+    O, O, X, X, X, X, O, O,
+    O, X, X, X, X, X, X, O,
+    X, X, O, X, X, O, X, X,
+    X, O, O, X, X, O, O, X,
+    O, O, O, X, X, O, O, O,
+    O, O, O, X, X, O, O, O,
+    O, O, O, X, X, O, O, O,
+]
+
+logo_down = [
+    O, O, O, X, X, O, O, O,
+    O, O, O, X, X, O, O, O,
+    O, O, O, X, X, O, O, O,    
+    X, O, O, X, X, O, O, X,
+    X, X, O, X, X, O, X, X,
+    O, X, X, X, X, X, X, O,
+    O, O, X, X, X, X, O, O,
+    O, O, O, X, X, O, O, O,
+]
+
+def lyt():
+  data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+  print ("received message:", data)
+
+def broadcast():
 	data = "Current temperature " + str(temp() + "Current humidity " + str(humi()))
-	s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
+	sock.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
 	print(data)
 	time.sleep(1)
 
-
-
-#Mulig løsning til hele koden på Raspberry Pi
-
-def lyt():
-	UDP_IP = "127.0.0.1"
-	UDP_PORT = 5005
-
-	sock = socket.socket(socket.AF_INET, # Internet
-						socket.SOCK_DGRAM) # UDP
-	sock.bind((UDP_IP, UDP_PORT))
-
-	while True:
-		data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-		print "received message:", data
-
-def broadcast():
-
-
-i = 0
-while True:
-	lyt()
-	if i == 3600:
-		broadcast()
-		i = 0
-	
-	if temp == optimal_temp and hum == optimal_hum:
-      print(" ")
-      s.clear()
+def compareValues(temp, hum, optimal_temp = 20, optimal_hum = 45):
+  if temp == optimal_temp and hum == optimal_hum:
+    print(" ")
+    s.clear()
   elif temp > optimal_temp or hum > optimal_hum:
     print("Over")
     s.set_pixels(logo_up)
@@ -70,6 +65,30 @@ while True:
   else:
     print("Fejl")
     s.clear(R)
-	sleep(1)
-	i += 1
 
+def main():
+  i = 0
+  while True:
+    #Måler temperatur
+    temp = s.get_temperature()
+    #Måler luftfugtighed
+    humi = s.get_humidity()
+    
+    temp = round(temp, 1)
+    humi = round(humi, 1)
+    
+    print("Temperature: " + str(temp) + " C")
+    print("Humidity: " + str(humi) + " %")
+    
+    lyt()
+    if i == 3600:
+      broadcast()
+      i = 0
+  
+    compareValues(temp, humi)
+    
+    sleep(1)
+    i += 1
+
+if __name__ == '__main__':
+  main()
