68bfbe5639dd97f953e191f1500a99fa6e5e137d
Nkragh <nikolaj.kragh@hotmail.com>
2019-12-11 13:04:06 +0100

    I don't even know man

--- GreenPiHouseSenseHat/BroadcastSender.py
+++ GreenPiHouseSenseHat/BroadcastSender.py
@@ -67,6 +67,9 @@ def broadcast(temp, humi):
   print(data)
   time.sleep(1)
 
+def Watering():
+  s.set_pixel(0,0,0,0,255)
+
 def compareValues(temp, hum):
   if temp == optimal_temp and hum == optimal_hum:
     print(" ")
@@ -110,3 +113,4 @@ def main():
 
 if __name__ == '__main__':
   main()
+  Watering()
