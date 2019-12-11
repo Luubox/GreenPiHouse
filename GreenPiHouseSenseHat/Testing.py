e17796b9d847b08541e8f117e5e2b02fd57a1653
NKragh <39466838+NKragh@users.noreply.github.com>
2019-12-11 12:42:02 +0100

    Det er bare læksikat nu ;i

--- GreenPiHouseSenseHat/Testing.py
+++ GreenPiHouseSenseHat/Testing.py
@@ -5,14 +5,15 @@ from datetime import datetime
 
 
 def RESTPost(value):
-  # url = 'https://thegreenerpihouse.azurewebsites.net/api/regulation'
-  url = 'http://localhost:61399/api/regulation'
-  nowtime =  str(datetime.now().date()) + "T" + str(datetime.now().time())
-  myobj = {"Timestamp": nowtime, "Status": value }
+  url = 'https://thegreenerpihouse.azurewebsites.net/api/regulation'
+  # url = 'http://localhost:61399/api/regulation'
+  nowtime =  str(datetime.now().date()) + "T" + str(datetime.now().time().replace(microsecond=0))
+  print(nowtime)
+  myobj = {'timestamp': nowtime, 'status': value }
   resp = requests.post(url, json=myobj)
   print(resp)
 
 if __name__ == "__main__":
-  RESTPost(1)
-  data = requests.get('http://localhost:61399/GetAllRegulations')
+  RESTPost(False)
+  data = requests.get('https://thegreenerpihouse.azurewebsites.net/GetAllRegulations')
   print(data.json())
